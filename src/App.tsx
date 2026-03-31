d1e82cd900d2047f29bcb833e1341c9b14763fa4
toto <toto.pp92831@gmail.com>
2026-03-31 11:27:32 +0900

    fix: corrected named imports in App.tsx to solve build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Dashboard from './components/Dashboard';
-import Sidebar from './components/Sidebar';
+import { Dashboard } from './components/Dashboard';
+import { Sidebar } from './components/Sidebar';
 import { AdminProvider } from './contexts/AdminContext';
 
 function App() {

20fe16e03c6e32af1a95a0b2b747f9b36b550cd7
toto <toto.pp92831@gmail.com>
2026-03-31 11:32:38 +0900

    fix: elevate AdminProvider to root level in main.tsx to solve white screen crash

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { Dashboard } from './components/Dashboard';
-import { Sidebar } from './components/Sidebar';
-import { AdminProvider } from './contexts/AdminContext';
 
 function App() {
   return (
-    <AdminProvider>
-      <div className="flex h-screen bg-gray-50 overflow-hidden">
-        <Sidebar />
-        <main className="flex-1 overflow-y-auto">
-          <Dashboard />
-        </main>
-      </div>
-    </AdminProvider>
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Dashboard />
+    </div>
   );
 }
 
